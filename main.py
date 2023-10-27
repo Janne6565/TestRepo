78c076c53576ceec5f4fbc8c8cbc5cd33d251775
Janne Keipert <jannekeipert@gmx.de>
2023-10-27 22:12:00 +0200

    test

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+
+url = "https://projektejwkk.de/_____mailGen/send.php"
+
+params = {
+    "author": "Janne",
+    "receiver": "jabbekeipert@gmail.com",
+    "message": "hallo, test",
+    "subject": "test"
+}
+
+requests.post(url, data=params)
+      
